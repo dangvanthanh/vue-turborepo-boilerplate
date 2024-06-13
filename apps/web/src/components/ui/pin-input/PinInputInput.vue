01d8227ea39ae0b469b4993727c9bed5caea997d
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-06-13 13:32:08 +0700

    deps: update shadcn-vue and biome

--- apps/web/src/components/ui/pin-input/PinInputInput.vue
+++ apps/web/src/components/ui/pin-input/PinInputInput.vue
@@ -1,24 +1,18 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-import {
-	PinInputInput,
-	type PinInputInputProps,
-	useForwardProps,
-} from 'radix-vue'
 import { type HTMLAttributes, computed } from 'vue'
+import { PinInputInput, type PinInputInputProps, useForwardProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
 
-const props = defineProps<
-	PinInputInputProps & { class?: HTMLAttributes['class'] }
->()
+const props = defineProps<PinInputInputProps & { class?: HTMLAttributes['class'] }>()
 
 const delegatedProps = computed(() => {
-	const { class: _, ...delegated } = props
-	return delegated
+  const { class: _, ...delegated } = props
+  return delegated
 })
 
 const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <PinInputInput v-bind="forwardedProps" :class="cn('flex w-10 h-10 text-center rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)" />
+  <PinInputInput v-bind="forwardedProps" :class="cn('relative text-center focus:outline-none focus:ring-2 focus:ring-ring focus:relative focus:z-10 flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md', props.class)" />
 </template>
